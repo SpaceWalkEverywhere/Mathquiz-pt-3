3dc55225dfc375d054f0dddffe0652ba105a9055
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-04-11 11:51:03 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,12 @@
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    ans= parseInt(number1)*parseInt(number2)
+    qno= "<h4>"+number1+" X "+number2+"</h4>";
+    ansbox="<br>Answer:<input type='text'id='checkbox'>";
+    checkbutt="<br><br><button class='btn btn-info' onclick='check();'>check</button>";
+    row=qno+ansbox+checkbutt;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("number1").value="";
+    document.getElementById("number2").value="";    
+}
